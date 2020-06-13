50d5c6971517120dcd960ec0ae3afd0f8404fcb1
Saya <172206333@qq.com>
2020-06-13 11:04:52 +0800

    登陆页面部分逻辑

--- lib/my/pages/ymt_login_page.dart
+++ lib/my/pages/ymt_login_page.dart
@@ -1,8 +1,30 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
 import '../../common/ymt_asset_path.dart';
 import '../../common/ymt_text_style.dart';
 
-class YMTLoginPage extends StatelessWidget {
+enum YMTLoginFieldType {
+  Message,
+  Password
+}
+
+class YMTLoginPage extends StatefulWidget {
+  YMTLoginPage({Key key}) : super(key: key);
+
+  @override
+  _YMTLoginPageState createState() => _YMTLoginPageState();
+}
+
+class _YMTLoginPageState extends State<YMTLoginPage> {
+  final StreamController<YMTLoginFieldType> _loginSwitchCtl = StreamController<YMTLoginFieldType>.broadcast();
+
+  @override
+  void dispose() {
+    _loginSwitchCtl.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,38 +36,54 @@ class YMTLoginPage extends StatelessWidget {
             color: Colors.white,
             child: Column(
               children: <Widget>[
-                Wrap(
-                  spacing: 80,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        GestureDetector(
-                          onTap: () {},
-                          child: Text(
-                            '快速登录',
-                            style: TextStyle(
-                              color: Colors.grey,
-                              decoration: TextDecoration.none,
-                              fontSize: 13,
-                              fontWeight: FontWeight.normal
+                StreamBuilder(
+                  stream: _loginSwitchCtl.stream,
+                  initialData: YMTLoginFieldType.Message,
+                  builder: (BuildContext context, AsyncSnapshot snapshot){
+                    return Container(
+                      child: Wrap(
+                        spacing: 80,
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              GestureDetector(
+                                onTap: () {
+                                  if (snapshot.data != YMTLoginFieldType.Message) {
+                                    _loginSwitchCtl.sink.add(YMTLoginFieldType.Message);
+                                  }
+                                },
+                                child: Text(
+                                  '快速登录',
+                                  style: TextStyle(
+                                    color: snapshot.data == YMTLoginFieldType.Message ? Colors.red : Colors.grey,
+                                    decoration: TextDecoration.none,
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.normal
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              if (snapshot.data != YMTLoginFieldType.Password) {
+                                _loginSwitchCtl.sink.add(YMTLoginFieldType.Password);
+                              }
+                            },
+                            child: Text(
+                              '密码登录',
+                              style: TextStyle(
+                                color: snapshot.data == YMTLoginFieldType.Password ? Colors.red : Colors.grey,
+                                decoration: TextDecoration.none,
+                                fontSize: 13,
+                                fontWeight: FontWeight.normal
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                    GestureDetector(
-                      onTap: () {},
-                      child: Text(
-                        '密码登录',
-                        style: TextStyle(
-                          color: Colors.grey,
-                          decoration: TextDecoration.none,
-                          fontSize: 13,
-                          fontWeight: FontWeight.normal
-                        ),
+                        ],
                       ),
-                    ),
-                  ],
+                    );
+                  },
                 ),
                 SizedBox(height: 15,),
                 Container(
@@ -54,87 +92,98 @@ class YMTLoginPage extends StatelessWidget {
                     border: Border.all(width: 1, color: Color(0xffe6e6e6)),
                     borderRadius: BorderRadius.circular(5),
                   ),
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        decoration: UnderlineTabIndicator(
-                          borderSide: BorderSide(width: 1, color: Color(0xffe6e6e6)),
-                          insets: EdgeInsets.only(left: 40, right: 12)
-                        ),
-                        child: Row(
+                  child: StreamBuilder(
+                    stream: _loginSwitchCtl.stream,
+                    initialData: YMTLoginFieldType.Message,
+                    builder: (BuildContext context, AsyncSnapshot snapshot){
+                      print('object');
+                      return Container(
+                        child: Column(
                           children: <Widget>[
                             Container(
-                              padding: EdgeInsets.only(left: 12, top: 3, right: 15),
-                              child: Image.asset(ic_mobile),
-                            ),
-                            Container(
-                              width: 200,
-                              child: TextField(
-                                style: TextStyle(fontSize: 15),
-                                decoration: InputDecoration.collapsed(
-                                  hintText: '手机号',
-                                  hintStyle: TextStyle(fontSize: 13),
-                                ),
+                              decoration: UnderlineTabIndicator(
+                                borderSide: BorderSide(width: 1, color: Color(0xffe6e6e6)),
+                                insets: EdgeInsets.only(left: 40, right: 12)
                               ),
-                            )
-                          ],
-                        ),
-                      ),
-                      Container(
-                        decoration: UnderlineTabIndicator(
-                          borderSide: BorderSide(width: 1, color: Color(0xffe6e6e6)),
-                          insets: EdgeInsets.only(left: 40, right: 12)
-                        ),
-                        child: Row(
-                          children: <Widget>[
-                            Container(
-                              padding: EdgeInsets.only(left: 12, top: 3, right: 12),
-                              child: Image.asset(ic_message),
-                            ),
-                            Expanded(
-                              child: TextField(
-                                style: TextStyle(fontSize: 15),
-                                decoration: InputDecoration.collapsed(
-                                  hintText: '短信验证码',
-                                  hintStyle: TextStyle(fontSize: 13),
-                                ),
+                              child: Row(
+                                children: <Widget>[
+                                  Container(
+                                    padding: EdgeInsets.only(left: 12, top: 3, right: 15),
+                                    child: Image.asset(ic_mobile),
+                                  ),
+                                  Container(
+                                    width: 200,
+                                    child: TextField(
+                                      keyboardType: TextInputType.phone,
+                                      style: TextStyle(fontSize: 15),
+                                      decoration: InputDecoration.collapsed(
+                                        hintText: snapshot.data == YMTLoginFieldType.Message ? '手机号' : '昵称/邮箱/手机号',
+                                        hintStyle: TextStyle(fontSize: 13),
+                                      ),
+                                    ),
+                                  )
+                                ],
                               ),
                             ),
                             Container(
-                              height: 30,
-                              decoration: BoxDecoration(
-                                border: Border(left: BorderSide(width: 1, color: Color(0xffe6e6e6)))
+                              decoration: UnderlineTabIndicator(
+                                borderSide: BorderSide(width: 1, color: Color(0xffe6e6e6)),
+                                insets: EdgeInsets.only(left: 40, right: 12)
                               ),
-                              padding: EdgeInsets.only(left: 3),
+                              child: Row(
+                                children: <Widget>[
+                                  Container(
+                                    padding: EdgeInsets.only(left: 12, top: 3, right: 12),
+                                    child: Image.asset(ic_message),
+                                  ),
+                                  Expanded(
+                                    child: TextField(
+                                      keyboardType: TextInputType.phone,
+                                      style: TextStyle(fontSize: 15),
+                                      decoration: InputDecoration.collapsed(
+                                        hintText: snapshot.data == YMTLoginFieldType.Message ? '短信验证码' : '登陆密码',
+                                        hintStyle: TextStyle(fontSize: 13),
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 30,
+                                    decoration: BoxDecoration(
+                                      border: Border(left: BorderSide(width: 1, color: Color(0xffe6e6e6)))
+                                    ),
+                                    padding: EdgeInsets.only(left: 3, right: snapshot.data == YMTLoginFieldType.Message ? 0 : 5),
+                                    child: FlatButton(
+                                      onPressed: () {},
+                                      child: Text(
+                                        snapshot.data == YMTLoginFieldType.Message ? '获取验证码' : '忘记密码',
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          color: Colors.black87
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Container(
+                              width: 300,
+                              padding: EdgeInsets.only(left: 12, top: 8, right: 12, bottom: 8),
                               child: FlatButton(
+                                color: Color(0xffe6e6e6),
                                 onPressed: () {},
                                 child: Text(
-                                  '获取验证码',
+                                  '登录',
                                   style: TextStyle(
-                                    fontSize: 12,
-                                    color: Colors.black87
+                                    color: Colors.white
                                   ),
                                 ),
                               ),
                             )
                           ],
                         ),
-                      ),
-                      Container(
-                        width: 300,
-                        padding: EdgeInsets.only(left: 12, top: 8, right: 12, bottom: 8),
-                        child: FlatButton(
-                          color: Color(0xffe6e6e6),
-                          onPressed: () {},
-                          child: Text(
-                            '登录',
-                            style: TextStyle(
-                              color: Colors.white
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
+                      );
+                    },
                   ),
                 ),
                 FlatButton(
@@ -170,10 +219,18 @@ class YMTLoginPage extends StatelessWidget {
               ],
             ),
           ),
-          Positioned(
-            top: 120,
-            left: 106,
-            child: Image.asset(ic_login_indicator,),
+          StreamBuilder(
+            stream: _loginSwitchCtl.stream,
+            initialData: YMTLoginFieldType.Message,
+            builder: (BuildContext context, AsyncSnapshot snapshot){
+              return Container(
+                child: Positioned(
+                  top: 120,
+                  left: snapshot.data == YMTLoginFieldType.Message ? 106 : 238,
+                  child: Image.asset(ic_login_indicator,),
+                ),
+              );
+            },
           ),
           Positioned(
             top: 25,
